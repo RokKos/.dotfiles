d5ef18a8c8778980dfcdd2ff8dec031ffd035b65
RokKosSloyd <rok@sloyd.ai>
2023-10-04 11:20:32 +0200

    First Setup: fish, bash, nvim, ssh

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+alias la='eza -la --git --git-repos --header'
+alias ls='eza -l --git --git-repos --header'
+alias cat='batcat'
